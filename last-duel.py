021fcaed05810fc1232b4c46fd3e39353af4b5b7
jamafaka <jamafaka@gmail.com>
2021-11-17 23:54:18 +0200

    sourcery r

--- last-duel.py
+++ last-duel.py
@@ -46,7 +46,7 @@ def main():
 #Defining amount of damage and miss chance for all the weapons
 def attack1(health2, name1):
     atq1 = input(name1 + " choose a weapon: ")
-    while atq1 != 'sword' and atq1 != 'axe' and atq1 != 'bow':
+    while atq1 not in ['sword', 'axe', 'bow']:
         print('Hey, choose a weapon among: sword, axe or bow')
         atq1 = input()
 #Sword mechanics
@@ -83,7 +83,7 @@ def attack1(health2, name1):
 #Defining amount of damage and miss chance  for all the weapons
 def attack2(health1, name2):
     atq2 = input(name2 + " choose a weapon: ")
-    while atq2 != 'sword' and atq2 != 'axe' and atq2 != 'bow':
+    while atq2 not in ['sword', 'axe', 'bow']:
         print('Hey, choose a weapon among: sword, axe or bow')
         atq2 = input()
 #Sword mechanics
